19ae376c501fd32f57b0cc7404b57dd50936e6e9
Arya Mukherjee <arya.mukherjee003@gmail.com>
2024-11-06 12:33:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// // Function to generate a random bright color
+// const colors = [
+//     "#66FF00",
+//     "#1974D2",
+//     "#08E8DE",
+//     "#FFF000",
+//     "#FFAA1D",
+//     "#FF007F",
+//     "#FFFFFF"
+// ];
+
+// // Function to randomly select a color from the predefined array
+// function getRandomColor() {
+//     const randomIndex = Math.floor(Math.random() * colors.length);
+//     return colors[randomIndex];
+// }
+
+// // Set random colors for each nav-left link's pseudo-element
+// document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
+//     link.style.setProperty('--random-color', getRandomColor());
+// });
+
+
+
+
+Shery.imageEffect(".image-container", { style: 5, config : {"a":{"value":2.98,"range":[0,30]},"b":{"value":-1,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":2.455371907267013},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":true},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":0.25,"range":[0.1,5]},"durationIn":{"value":0.32,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":false},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":false},"onMouse":{"value":0},"noise_speed":{"value":0.23,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2],"_gsap":{"id":3}},"discard_threshold":{"value":0.69,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.53,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}}  , 
+     gooey: true })
+
